4ae36dcab44ab88bd9193118b8336aaa0e44a34a
Jax <jiaxingrong66@gmail.com>
2026-04-20 11:38:00 -0400

    added new dungeon asset

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ class_name Player
 
 var last_direction: Vector2 = Vector2.DOWN
 
+
 func update_last_direction(direction: Vector2):
 	if direction != Vector2.ZERO:
 		if abs(direction.x) > abs(direction.y):
