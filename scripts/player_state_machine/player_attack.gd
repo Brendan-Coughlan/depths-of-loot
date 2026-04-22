78c9e68912f55cea794b517a73b959d941b100d9
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-04-22 15:53:54 -0400

    Changed shroom to skeleton and implemented all hitboxes

--- scripts/player_state_machine/player_attack.gd
+++ scripts/player_state_machine/player_attack.gd
@@ -3,22 +3,33 @@ class_name PlayerAttack
 
 @export var player: Player
 @export var player_sprite: AnimatedSprite2D
-@export var player_hitbox: CollisionShape2D
+@export var player_hitboxes: Array[CollisionShape2D]
 
 func enter() -> void:
-	# Update last_direction if player is pressing a movement key
-	var direction := Input.get_vector(
-		"move_left", "move_right",
-		"move_up", "move_down"
-	)
-
-	if direction != Vector2.ZERO:
-		player.update_last_direction(direction)
+	var dir := player.last_direction
 
 	play_attack_animation()
-	player_hitbox.disabled = false
+	if abs(dir.x) > abs(dir.y):
+		if dir.x < 0:
+			player_hitboxes[1].disabled = false
+		else:
+			player_hitboxes[0].disabled = false
+	else:
+		if dir.y < 0:
+			player_hitboxes[2].disabled = false
+		else:
+			player_hitboxes[3].disabled = false
 	await player_sprite.animation_finished
-	player_hitbox.disabled = true
+	if abs(dir.x) > abs(dir.y):
+		if dir.x < 0:
+			player_hitboxes[1].disabled = true
+		else:
+			player_hitboxes[0].disabled = true
+	else:
+		if dir.y < 0:
+			player_hitboxes[2].disabled = true
+		else:
+			player_hitboxes[3].disabled = true
 	Transitioned.emit(self, "idle")
 
 func physics_update(_delta: float) -> void:
