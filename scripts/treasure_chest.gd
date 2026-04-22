0b274389f579e9534c90b853821fd4555f4dffe1
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-04-22 19:51:53 -0400

    Added gold bar item and modified treasure chest

--- scripts/treasure_chest.gd
+++ scripts/treasure_chest.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 @onready var interactable: Area2D = $Interactable
 @onready var treasure_chest_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var gold_key_item : ItemData
+@export var dropped_item : ItemData
 
 func _ready() -> void:
 	interactable.interact = _on_interact
@@ -12,7 +12,7 @@ func _on_interact():
 	if treasure_chest_sprite.animation == "closed_chest":
 		treasure_chest_sprite.play("opened_chest")
 		interactable.is_interactable = false
-		spawn_item(gold_key_item, 1)
+		spawn_item(dropped_item, 1)
 		
 func spawn_item(item: ItemData, amount: int):
 	var instance = preload("res://scenes/item_pickup.tscn").instantiate()
