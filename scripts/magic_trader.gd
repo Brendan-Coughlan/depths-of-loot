d63c70903251bcd5cadad79e3628503460b7b0d7
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-05-06 10:05:33 -0400

    Added equipment and magic trader (WIP)

---
+++ scripts/magic_trader.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+@onready var interactable: Area2D = $Interactable
+@onready var trader_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var shop_menu: Shop = $Shop
+
+func _ready() -> void:
+	interactable.interact = _on_interact
+	
+func _on_interact():
+	shop_menu.toggle_window(true)
