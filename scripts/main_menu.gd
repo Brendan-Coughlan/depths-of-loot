e4151ba473d0e76d4f947498fdb3127874a4bf41
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-05-09 12:44:27 -0400

    Changed main menu to match style

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,8 +9,8 @@ func _process(delta: float) -> void:
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func _on_options_pressed() -> void:
+func _on_settings_pressed() -> void:
 	print("Options pressed")
 
-func _on_exit_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
