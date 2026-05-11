2c201c2a267e38c951da30221a9743b48e7218e7
Jax <jiaxingrong66@gmail.com>
2026-05-11 17:30:36 -0400

    fixed mini-map

--- scripts/map/mini_map_camera.gd
+++ scripts/map/mini_map_camera.gd
@@ -3,9 +3,19 @@ extends Camera2D
 @export var player: Node2D
 
 func _ready() -> void:
+	resolve_player()
 	enabled = true
 	zoom = Vector2(0.25, 0.25)
 
 func _process(_delta: float) -> void:
-	if player:
-		global_position = player.global_position
+	if not is_instance_valid(player):
+		resolve_player()
+
+	if player == null:
+		return
+
+	global_position = player.global_position
+
+
+func resolve_player() -> void:
+	player = get_tree().get_first_node_in_group("player") as Node2D
